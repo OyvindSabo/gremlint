74948c04aa686c8ad51328f08f2b8e521465f157
oyvindsabo <oyvind.sabo@gmail.com>
2020-03-23 01:01:58 +0100

    Create functioning test script

--- test/test.js
+++ test/test.js
@@ -1,12 +1,91 @@
-const test = (testTitle, ...assertions) => {
-  console.log('running test');
+const green = text => `\x1b[32m${text}\x1b[0m`;
+const red = text => `\x1b[31m${text}\x1b[0m`;
+const yellow = text => `\x1b[33m${text}\x1b[0m`;
+
+/**
+ *
+ * @param {...{ title: string; assertions: { assertionDidPass: boolean; explanation: string }; assertionsRun: number; assertionsPassed: number, testDidPass }} tests
+ */
+const runTests = (...tests) => {
+  const testsRun = tests.length;
+  const testsPassed = tests.filter(({ testDidPass }) => testDidPass).length;
+  tests.forEach(
+    ({ title, assertions, assertionsRun, assertionsPassed, testDidPass }) => {
+      console.log(
+        '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+      );
+      console.log(title);
+      console.log(
+        '━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+      );
+      assertions.forEach(({ assertionDidPass, explanation }) => {
+        console.log(assertionDidPass ? green(explanation) : red(explanation));
+      });
+      const fractionOfAssertionsPassedDescription = `\n${assertionsPassed}/${assertionsRun} assertions passed`;
+      console.log(
+        testDidPass
+          ? green(fractionOfAssertionsPassedDescription)
+          : red(fractionOfAssertionsPassedDescription)
+      );
+    }
+  );
+  const fractionOfTestsPassedDescription = `${testsPassed}/${testsRun} tests passed`;
+  const allTestsPassed = testsPassed === testsRun;
+  const noTestsPassed = !testsPassed;
+  console.log(
+    '\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  );
+  console.log('Summary');
+  console.log(
+    '━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+  );
+  console.log(
+    allTestsPassed
+      ? green(`All your tests passed:)\n${fractionOfTestsPassedDescription}`)
+      : noTestsPassed
+      ? red(`All your tests failed:(\n${fractionOfTestsPassedDescription}`)
+      : yellow(
+          `Some of your tests did not pass\n${fractionOfTestsPassedDescription}`
+        )
+  );
 };
-const assertEquals = () => {
-  console.log('running assertEquals');
-  return true;
+
+/**
+ * @param {string} title
+ * @param {...{ assertionDidPass: boolean; explanation: string }} assertions
+ */
+const test = (title, ...assertions) => {
+  // For each test, first print the title
+  const assertionsRun = assertions.length;
+  const assertionsPassed = assertions.filter(
+    ({ assertionDidPass }) => assertionDidPass
+  ).length;
+  return {
+    title,
+    assertions,
+    assertionsRun,
+    assertionsPassed,
+    testDidPass: assertionsPassed === assertionsRun,
+  };
+};
+
+/**
+ * @param {any} expected
+ * @param {any} actual
+ * @returns {{ assertionDidPass: boolean; explanation: string }}
+ */
+const assertEquals = (expected, actual) => {
+  const assertionDidPass = expected === actual;
+  return {
+    assertionDidPass,
+    explanation: assertionDidPass
+      ? 'Assertion passed'
+      : `Assertion failed:\nExpected:\n${expected}\nActual:\n${actual}`,
+  };
 };
 
 module.exports = {
+  runTests,
   test,
   assertEquals,
 };
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,26 @@
-const { test, assertEquals } = include('test/test.js');
+const { runTests, test, assertEquals } = include('test/test.js');
 
-test(
-  'Queries with less than 80 characters should be formatted to a single line',
-  assertEquals(
-    `g.V().\nhasLabel('Application')`,
-    `g.V().hasLabel('Application')`
+runTests(
+  test(
+    'Queries with less than 80 characters should be formatted to a single line',
+    assertEquals(
+      `g.V().hasLabel('Application')`,
+      `g.V().hasLabel('Application')`
+    )
+  ),
+  test(
+    'Queries with less than 80 characters should be formatted to a single line',
+    assertEquals(
+      `g.V().hasLabel('Application')`,
+      `g.V().hasLabel('Application')`
+    ),
+    assertEquals(
+      `g.V().hasLabel('Application')`,
+      `g.V().hasLabel('Application')`
+    ),
+    assertEquals(
+      `g.V() .hasLabel('Application')`,
+      `g.V().hasLabel('Application')`
+    )
   )
 );

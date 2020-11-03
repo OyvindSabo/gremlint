60b96309ce54be78cf6b561a66c169dd8559713b
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-03 23:10:30 +0100

    Add by()-modulator wrapping tests

--- src/formatQuery/__tests__/modulatorWrapping.test.ts
+++ src/formatQuery/__tests__/modulatorWrapping.test.ts
@@ -46,4 +46,64 @@ test("Modulators should not be line-wrapped if they can fit on the line of the s
   select('person', 'location').
     by('name').
     by(valueMap())`);
+
+  // Test by()-modulator wrapping
+  expect(
+    formatQuery('g.V().group().by().by(bothE().count())', {
+      indentation: 0,
+      maxLineLength: 40,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe('g.V().group().by().by(bothE().count())');
+  expect(
+    formatQuery('g.V().group().by().by(bothE().count())', {
+      indentation: 0,
+      maxLineLength: 35,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe(`g.V().
+  group().by().by(bothE().count())`);
+  expect(
+    formatQuery('g.V().group().by().by(bothE().count())', {
+      indentation: 0,
+      maxLineLength: 30,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe(`g.V().
+  group().
+    by().
+    by(bothE().count())`);
+  expect(
+    formatQuery(
+      "g.V().hasLabel('person').group().by(values('name', 'age').fold()).unfold().filter(select(values).count(local).is(gt(1)))",
+      {
+        indentation: 0,
+        maxLineLength: 45,
+        shouldPlaceDotsAfterLineBreaks: false,
+      },
+    ),
+  ).toBe(`g.V().
+  hasLabel('person').
+  group().by(values('name', 'age').fold()).
+  unfold().
+  filter(
+    select(values).count(local).is(gt(1)))`);
+  expect(
+    formatQuery(
+      "g.V().hasLabel('person').group().by(values('name', 'age').fold()).unfold().filter(select(values).count(local).is(gt(1)))",
+      {
+        indentation: 0,
+        maxLineLength: 40,
+        shouldPlaceDotsAfterLineBreaks: false,
+      },
+    ),
+  ).toBe(`g.V().
+  hasLabel('person').
+  group().
+    by(values('name', 'age').fold()).
+  unfold().
+  filter(
+    select(values).
+    count(local).
+    is(gt(1)))`);
 });

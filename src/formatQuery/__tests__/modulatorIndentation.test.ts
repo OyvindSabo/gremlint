97edc9226e2cb1412c98b806118f538b16fd8fa1
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-03 21:50:09 +0100

    Add emit()-modulator indentation tests

--- src/formatQuery/__tests__/modulatorIndentation.test.ts
+++ src/formatQuery/__tests__/modulatorIndentation.test.ts
@@ -75,4 +75,62 @@ test('Wrapped modulators should be indented with two spaces', () => {
         is(lt(28)),
         constant('young'),
         choose(is(lt(30)), constant('old'), constant('very old'))))`);
+
+  // Test emit()-modulator indentation
+  expect(
+    formatQuery("g.V(1).repeat(bothE('created').dedup().otherV()).emit().path()", {
+      indentation: 0,
+      maxLineLength: 45,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe(
+    `g.V(1).
+  repeat(bothE('created').dedup().otherV()).
+    emit().
+  path()`,
+  );
+
+  expect(
+    formatQuery('g.V().repeat(both()).times(1000000).emit().range(6,10)', {
+      indentation: 0,
+      maxLineLength: 35,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe(
+    `g.V().
+  repeat(both()).
+    times(1000000).
+    emit().
+  range(6, 10)`,
+  );
+
+  expect(
+    formatQuery("g.V(1).repeat(out()).times(2).emit().path().by('name')", {
+      indentation: 0,
+      maxLineLength: 30,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe(
+    `g.V(1).
+  repeat(out()).
+    times(2).
+    emit().
+  path().by('name')`,
+  );
+
+  expect(
+    formatQuery("g.withSack(1).V(1).repeat(sack(sum).by(constant(1))).times(10).emit().sack().math('sin _')", {
+      indentation: 0,
+      maxLineLength: 40,
+      shouldPlaceDotsAfterLineBreaks: false,
+    }),
+  ).toBe(
+    `g.withSack(1).
+  V(1).
+  repeat(sack(sum).by(constant(1))).
+    times(10).
+    emit().
+  sack().
+  math('sin _')`,
+  );
 });

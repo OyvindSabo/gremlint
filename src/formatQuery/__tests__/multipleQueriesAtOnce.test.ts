71daa3723459d792d56f4690cd5fa15c67ec4dca
oyvindsabo <oyvind.sabo@gmail.com>
2020-11-06 00:39:28 +0100

    Add another test for formatting multiple queries at once
    - This test tests that linebreaks don't happen too soon because the
      formatter fails to distinguish between the lines from the end of one
      query and the start of the next.

--- src/formatQuery/__tests__/multipleQueriesAtOnce.test.ts
+++ src/formatQuery/__tests__/multipleQueriesAtOnce.test.ts
@@ -1,6 +1,27 @@
 import { formatQuery } from '..';
 
 test('Each of multiple queries formatted at once should be formatted as if they were formatted individually', () => {
+  // Test that linebreaks don't happen too soon because the formatter fails to distinguish between lines from the end
+  // of one query and the start of the next
+  expect(
+    formatQuery(
+      `g.V(1).out().values('name')
+
+g.V(1).out().map{ it.get().value('name') }
+
+g.V(1).out().map(values('name'))`,
+      {
+        indentation: 0,
+        maxLineLength: 45,
+        shouldPlaceDotsAfterLineBreaks: false,
+      },
+    ),
+  ).toBe(`g.V(1).out().values('name')
+
+g.V(1).out().map{ it.get().value('name') }
+
+g.V(1).out().map(values('name'))`);
+
   expect(
     formatQuery(
       `g.V().branch{ it.get().value('name') }.option('marko', values('age')).option(none, values('name'))
